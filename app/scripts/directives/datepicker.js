e003996c9c633c78082cf2656865cc4ba07f677a
Toshiyuki HINA <thina@yourinventit.com>
2014-07-16 10:05:27 +0900

    refactor

--- app/scripts/directives/datepicker.js
+++ app/scripts/directives/datepicker.js
@@ -2,44 +2,46 @@
 
 angular.module('angularDatepickerApp')
   .directive('datepicker', ['$log', function ($log) {
+    // 'ngModel'(4th argument) is set by 'require' property.
+    var link = function(scope, element, attrs, ngModel) {
+      $log.debug('* link');
+      
+      var updateModel = function(dateText) {
+        $log.debug('* updateModel');
+        scope.$apply(function() {
+          $log.debug('* $apply@updateModel');
+          ngModel.$setViewValue(dateText);
+        });
+      };
+
+      ngModel.$render = function() {
+        $log.debug('* $render');
+        element.datepicker('setDate', ngModel.$viewValue || '');
+      };
+
+      var optionsObj = {};
+      optionsObj.dateFormat = 'mm/dd/yy';
+      optionsObj.onSelect = function(dateText, picker) {
+        $log.debug('* onSelect');
+        updateModel(dateText);
+        if (scope.select) {
+          scope.$apply(function() {
+            $log.debug('* $apply@onSelect');
+            // Invokes '$scope.updateMyText()' via select.
+            scope.select({ date: dateText });
+          });
+        }
+      };
+
+      element.datepicker(optionsObj);
+    };
+
     return {
       restrict: 'A',
       require: 'ngModel',
       scope: {
         select: '&'
       },
-      link: function postLink(scope, element, attrs, ngModel) {
-        var optionsObj = {};
-
-        optionsObj.dateFormat = 'mm/dd/yy';
-        optionsObj.autoSize = false;
-
-        var updateModel = function(dateText) {
-          $log.debug('* updateModel');
-          scope.$apply(function() {
-            $log.debug('* $apply@updateModel');
-            ngModel.$setViewValue(dateText);
-          });
-        };
-
-        optionsObj.onSelect = function(dateText, picker) {
-          $log.debug('* onSelect');
-          updateModel(dateText);
-          if (scope.select) {
-            scope.$apply(function() {
-              $log.debug('* $apply@onSelect');
-              // Invokes '$scope.updateMyText()' via select.
-              scope.select({date: dateText});
-            });
-          }
-        };
-
-        ngModel.$render = function() {
-          $log.debug('* $render');
-          element.datepicker('setDate', ngModel.$viewValue || '');
-        };
-
-        element.datepicker(optionsObj);
-      }
+      link: link
     };
   }]);
